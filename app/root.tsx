1fe4e48f833a89947a8717953b4639d570e4d8be
Matthias Martin <github@stichwort-m.de>
2024-02-06 16:59:45 +0100

    initial commit

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,8 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
 } from "@remix-run/react";
 
 export const links: LinksFunction = () => [
@@ -26,6 +28,74 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  // when true, this is what used to go to `CatchBoundary`
+  if (isRouteErrorResponse(error)) {
+    return (
+      <html lang="de" className="bg-background text-gray-950 scroll-smooth">
+        <head>
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          <div className="max-w-2xl px-4 mx-auto my-16 text-center xs:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold xxxs:text-4xl">
+              {error.status === 404 ? 'Diese Seite existiert nicht' : 'Ein Fehler ist aufgetreten'}
+            </h1>
+            <p className="mt-2 text-lg text-gray-500 uppercase">Status: {error.status}</p>
+            {error.status !== 404 && <p className="mt-8 text-xl font-medium">{error.data.message}</p>}
+            <a
+              className="block px-4 py-3 mt-12 font-medium text-center text-white bg-blue-500 rounded-lg shrink-0 transition-colors duration-200 hover:bg-blue-600"
+              href="/"
+              title="Zur Startseite"
+            >
+              Zur Startseite
+            </a>
+          </div>
+          <ScrollRestoration />
+          <LiveReload />
+        </body>
+      </html>
+    );
+  }
+
+  // Don't forget to typecheck with your own logic.
+  // Any value can be thrown, not just errors!
+  const errorMessage =
+    error && typeof error === 'object' && 'message' in error && error.message && typeof error.message === 'string'
+      ? error.message
+      : 'Unknown error';
+
+  return (
+    <html lang="de" className="bg-background text-gray-950 scroll-smooth">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Links />
+      </head>
+      <body>
+        <div className="max-w-2xl px-4 mx-auto my-16 text-center xs:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold xxxs:text-4xl">Ein Fehler ist aufgetreten</h1>
+          <p className="mt-8 text-xl font-medium">{errorMessage}</p>
+          <a
+            className="block px-4 py-3 mt-12 font-medium text-center text-white bg-blue-500 rounded-lg shrink-0 transition-colors duration-200 hover:bg-blue-600"
+            href="/"
+            title="Zur Startseite"
+          >
+            Zur Startseite
+          </a>
+        </div>
+        <ScrollRestoration />
         <LiveReload />
       </body>
     </html>
---
+++ app/routes/test.tsx
@@ -0,0 +1,102 @@
+import type { LoaderFunction, MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Test() {
+  return (
+    <div
+      style={{
+        fontFamily: "system-ui, sans-serif",
+        lineHeight: "1.8",
+        maxWidth: "30em",
+        margin: "auto",
+      }}
+    >
+      <h1>Test Route</h1>
+      <div>
+        <p>
+          <Link to="/">Go to home route</Link>
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <h2>Highlight</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+  );
+}
